8c29fe3af566191b8ad4fb0944c5281361d1bfa2
MarketingPlus <benmarketingplus@gmail.com>
2021-03-12 17:13:41 +1100

    created variables to link elements to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
+// input types and linking them in js
+
 const characterSliderRange = document.getElementById('characterSliderRange')
 const numberBoxNumber = document.getElementById('numberBoxNumber')
 
+const uppercaseElement = document.getElementById('uppercase')
+const lowercaseElement = document.getElementById('lowercase')
+const symbolsElement = document.getElementById('symbols')
+const numbersElement = document.getElementById('numbers')
+
+const button = document.getElementById('generate')
+
+
 characterSliderRange.addEventListener('input', linkCharacters)
 numberBoxNumber.addEventListener('input', linkCharacters)
+
+button.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterSlider = numberBoxNumber.value
+  const uppercase = uppercaseElement.checked
+  const lowercase = lowercaseElement.checked
+  const symbols = symbolsElement.checked
+  const numbers = numbersElement.checked
+  const password = generatePassword(characterSlider, uppercase, lowercase, symbols, numbers)
+})
 
 function linkCharacters(e) {
   const value = e.target.value
@@ -11,16 +31,4 @@ function linkCharacters(e) {
 }
 
 
-var generateBtn = document.querySelector("#generate");
-
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
-generateBtn.addEventListener("click", writePassword);
+// var generateBtn = document.querySelector("#generate");function writePassword() {var password = generatePassword();var passwordText = document.querySelector("#password");passwordText.value = password;}generateBtn.addEventListener("click", writePassword);
