c027fbfdaf2f244a60c5959df1300e654fb8ed55
MarketingPlus <benmarketingplus@gmail.com>
2021-03-15 16:00:21 +1100

    Completely commented on my JS file to explain what each function or varible does so I know what I can improve on or fix if errors arrise

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const characterSliderRange = document.getElementById('characterSliderRange')
 const numberBoxNumber = document.getElementById('numberBoxNumber')
 
-// all my check boxes and linking them
+// all my checkboxes and linking them to a varible
 const uppercaseElement = document.getElementById('uppercaseVal')
 const lowercaseElement = document.getElementById('lowercaseVal')
 const symbolsElement = document.getElementById('symbolsVal')
@@ -19,14 +19,18 @@ const clipboard = document.getElementById('clipboard')
 
 // copy to clipboard function
 clipboard.addEventListener('click', e => {
+  // varible to be able to select the password and perform a command on it
   const textOutput = document.getElementById('passwordShow')
 
+  // command to select the password and copy it to te clipboard
   textOutput.select();
   document.execCommand('copy');
+
+  // an alert box allowing the individual to be notified when the password has been copied
   window.alert("You have successfully copied your UNIQUE password to your clipboard 📋")
 })
 
-// creating arrays for the special characters so i dont have to manually type each code i can justr convert the code number to character
+// creating arrays for the special characters so i dont have to manually type each code, i can just convert the ASII code to the actual character
 const UPPERCASE_CHAR = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR = arrayFromLowToHigh(97, 122)
 const NUMBERS_CHAR = arrayFromLowToHigh(48, 57)
@@ -38,7 +42,7 @@ const SYMBOLS_CHAR = arrayFromLowToHigh(33, 47).concat(
   arrayFromLowToHigh(123, 126)
 )
 
-// linking my slider and number so they both have the same value
+// linking my slider and number so they both have the same value allowing the process to move a lot smoother than have two different values which would break the code
 characterSliderRange.addEventListener('input', linkCharacters)
 numberBoxNumber.addEventListener('input', linkCharacters)
 
@@ -49,32 +53,43 @@ button.addEventListener('click', e => {
   const lowercaseVal = lowercaseElement.checked
   const symbolsVal = symbolsElement.checked
   const numbersVal = numbersElement.checked
+
+  // creating a variable callled password which is equal to the function generatePassword
   const password = generatePassword(characterNumber, uppercaseVal, lowercaseVal, symbolsVal, numbersVal)
+
+  // printing the generated password into the html so users can see/ use it
   passwordShow.innerText = password
 });
 
 
-// my function to generate the password which includes all my different variables 
+// my function to generate the password which includes all my different variables/ user inputs 
 function generatePassword(characterNumber, uppercaseVal, lowercaseVal, symbolsVal, numbersVal) {
+  // setting the value of the password to nothing so i can concat the varible charPass to 
   let password = '';
 
+  // setting a value so users can't create a password with no inputs selected
   const valCount = lowercaseVal + uppercaseVal + numbersVal + symbolsVal;
 
+  // this checks if users have checked the checkboxes if true it concats the random value generated into the array
   let charPass = [];
   if (uppercaseVal) charPass = charPass.concat(UPPERCASE_CHAR)
   if (lowercaseVal) charPass = charPass.concat(LOWERCASE_CHAR)
   if (symbolsVal) charPass = charPass.concat(SYMBOLS_CHAR)
   if (numbersVal) charPass = charPass.concat(NUMBERS_CHAR)
 
+  // checking if the varible is unchecked, if all checkboxes are unchecked this message will be displayed
   if(valCount === 0) {
     return 'Please select atlease one option 👻';
   }
   
+  // loop to convert the password with ASII characters to actual characters.
   const passwordCharacters = []
   for (let i = 0; i < characterNumber; i++) {
     const characterCode = charPass[Math.floor(Math.random() * charPass.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
+
+  // returns the converted output into passwordCharacters which is stored into an array which will be passed through the variable
   return passwordCharacters.join('')
 }
 
